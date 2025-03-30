49d70aa062f7b9db3f06c2872b4cb47ae26dc943
Fran Barea <fbarea@qalea.ai>
2025-03-30 12:02:03 +0200

    adding cecilio

--- settings.ts
+++ settings.ts
@@ -1,12 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
-const { BERNARDO_PASS, BERNARDO_USER } = process.env
+const { BERNARDO_PASS, BERNARDO_USER, CECILIO_PASS, CECILIO_USER } = process.env
 
 export default {
   USERS: [
     {
       password: BERNARDO_PASS || '',
       username: BERNARDO_USER || '',
+    },
+    {
+      password: CECILIO_PASS || '',
+      username: CECILIO_USER || '',
     },
   ],
 }
